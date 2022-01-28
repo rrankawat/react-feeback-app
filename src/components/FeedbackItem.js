c140ef5ecb2eb2415590a4fe2bc297dcac208f5f
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-01-28 13:04:13 +0530

    Events & Props Drilling

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,10 +1,14 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button className='close' onClick={() => handleDelete(item.id)}>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
@@ -12,6 +16,7 @@ const FeedbackItem = ({ item }) => {
 
 FeedbackItem.propTypes = {
   item: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 }
 
 export default FeedbackItem
