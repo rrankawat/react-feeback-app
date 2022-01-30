9df8c60880dd9187735e865451bc5d26800d73cc
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-01-30 22:20:33 +0530

    React Router Dom v6

---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,17 @@
+import Card from '../shared/Card'
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <h1>About This Project</h1>
+      <p>This is a React app to leave feedback for a product or a service</p>
+      <p>Version: 1.0.0</p>
+
+      <p>
+        <a href='/'>Home</a>
+      </p>
+    </Card>
+  )
+}
+
+export default AboutPage
