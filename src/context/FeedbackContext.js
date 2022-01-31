c3bec829ebbcbe2785cef9014fb97c0cfbea6753
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-01-31 18:09:02 +0530

    Update & Delete

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -35,8 +35,10 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([data, ...feedback])
   }
 
-  const deleteFeedback = id => {
+  const deleteFeedback = async id => {
     if (window.confirm('Are you sure want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
       setFeedback(feedback.filter(item => item.id !== id))
     }
   }
@@ -48,9 +50,18 @@ export const FeedbackProvider = ({ children }) => {
     })
   }
 
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const res = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+    const data = await res.json()
+
     setFeedback(
-      feedback.map(item => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map(item => (item.id === id ? { ...item, ...data } : item))
     )
   }
 
